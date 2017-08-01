edb1bfedb1d3757281f8fbbe184537bdb299188f
Bilel MEMDOUH <bilel.memdouh@savoirfairelinux.com>
2017-08-01 15:31:02 -0400

    With ceremony

--- functionnal/src/main/java/com/bilelovic/App.java
+++ functionnal/src/main/java/com/bilelovic/App.java
@@ -1,6 +1,10 @@
 package com.bilelovic;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
 import java.util.function.IntPredicate;
+import java.util.function.Predicate;
 import java.util.stream.IntStream;
 
 /**
@@ -11,11 +15,42 @@ public class App
 {
     public static void main( String[] args )
     {
-    	testIsPrime();
-        
+    	//testIsPrime();
+        doubleFirstEvenNumber();
     }
 
     
+	private static void doubleFirstEvenNumber() {
+		//find the double of the first even number greater than 3
+		List<Integer> values = Arrays.asList(1,2,3,5,4,6,7,8,9,10);
+		Predicate<Integer> isGreaterThan3 = number -> number > 3;
+		// function ? 
+		Function<Integer, Predicate<Integer>> isGreaterThan = pivot -> 
+				number -> number > pivot;
+		System.out.println(
+		values.stream()
+				.filter(isGreaterThan.apply(4))
+				.filter(App::isEven)
+				.map(App::doubleIt)
+				.findFirst()
+				.get()
+				);
+	}
+
+
+	private static boolean isGreaterThan3(int e) {
+		System.out.println("IsGreaterThan "+ e);
+		return e > 3;
+	}
+
+	private static boolean isEven(int e) {
+		System.out.println("isEven "+ e);
+		return e % 2 == 0;
+	}
+	private static int doubleIt(int e) {
+		return e*2;
+	}
+
 	private static void testIsPrime() {
 		System.out.println(isPrime(1));
         System.out.println(isPrime(2));
